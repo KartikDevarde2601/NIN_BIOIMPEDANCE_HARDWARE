57c315182002fde62c056dc4e17d97aad48e95ce
kartik satish devarde <kartikdevarde@outlook.com>
2025-02-23 20:27:52 +0530

    change machine

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <PicoMQTT.h>
-#include "time.h"
 #include <ESP32Time.h>
+#include <PicoMQTT.h>
+#include <WiFi.h>
+#include <vector>
+#include <unordered_map>
+#include <string>
 
 // RTC instance
 ESP32Time rtc;
 
-// NTP Server
-const char *ntpServer = "pool.ntp.org"; // Alternative: "time.google.com"
-const long gmtOffset_sec = 19800;       // GMT+5:30 (India Standard Time)
-const int daylightOffset_sec = 0;
-
 // WiFi Credentials
 const char *WIFI_SSID = "Galaxy";
 const char *WIFI_PASS = "kartik2001";
+
+// configaration variable
+
+std::vector<int> frequecies;
+std::vector<std::string> UpperBody {}
+std::unordered_map<int, std::vector<int>> cofig;
 
 // MQTT Configuration
 PicoMQTT::Server mqtt;
@@ -43,38 +46,18 @@ void setup()
   Serial.println("WiFi connected");
   Serial.println("IP address: " + WiFi.localIP().toString());
 
+  mqtt.subscribe("global/time", [](const char *topic, const char *payload)
+                 { unsigned long unixTime = strtoul(payload, nullptr, 10); });
+
+  rtc.setTime(1740146300, 0);
+
   mqtt.begin();
-
-  // Get Timestamp (with retry)
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  struct tm timeinfo;
-
-  int retry = 0;
-  while (!getLocalTime(&timeinfo) && retry < 10) // Retry up to 10 times
-  {
-    Serial.println("Waiting for NTP time sync...");
-    delay(1000);
-    retry++;
-  }
-
-  if (retry >= 10)
-  {
-    Serial.println("Failed to obtain time after retries.");
-    return;
-  }
-
-  // Get epoch time and set RTC
-  time_t now;
-  time(&now);
-  rtc.setTime(now); // Sync RTC with NTP time
-  Serial.println("RTC Time Set from NTP");
-
-  // Print the current RTC time
-  unsigned long currentTime = rtc.getLocalEpoch();
-  Serial.println(currentTime);
 }
 
 void loop()
 {
   mqtt.loop();
+
+  delay(2000);
+  Serial.println(rtc.getEpoch());
 }
